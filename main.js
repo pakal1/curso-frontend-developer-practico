68644deb5df299acd77744e7d03056032142bf0f
DEX <hontzawf@gmail.com>
2023-05-03 14:57:52 -0300

    vinculamos Mobile-menu a Index

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMneu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
-    desktopMneu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive');
 
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
