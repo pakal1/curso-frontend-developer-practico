f4486649b7f4591d7b2f492fe8b8cda26c35777d
DEX <hontzawf@gmail.com>
2023-05-10 14:53:53 -0300

    agregamos info de producto en el carrito

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 const mobileMenu = document.querySelector('.mobile-menu');
 
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 const cardsContainer = document.querySelector('.cards-container');
 
@@ -18,10 +18,10 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -29,10 +29,10 @@ function toggleDesktopMenu(){
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed) {
-        aside.classList.add('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
 
